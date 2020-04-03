dfe46c6fd0192728059450d2d96b84dfa7407d73
Dan Weikart <danmweikart@gmail.com>
2020-04-02 17:29:08 -0700

    created for each function to populate hour, description blocks. Will not populate [0].

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var currentDayEl = $("#currentDay");
-// var hourEl = $(".hour");
-// var descriptionEl = $(".description")
+var hourEl = $(".hour");
+var descriptionEl = $(".description")
 
 var currentHour = moment().format("HH");
 var currentFullDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -10,54 +10,60 @@ currentDayEl.html(currentFullDate);
 var timeChunks = [
     {
         hourBlock: 7,
-        schedItem: ""
+        schedItem: "new item at 7"
     },
     {
         hourBlock: 8,
-        schedItem: ""
+        schedItem: "new item at 8"
     },
     {
         hourBlock: 9,
-        schedItem: ""
+        schedItem: "new item at 9"
     },
     {
         hourBlock: 10,
-        schedItem: ""
+        schedItem: "new item at 10"
     },
     {
         hourBlock: 11,
-        schedItem: ""
+        schedItem: "new item at 11"
     },
     {
         hourBlock: 12,
-        schedItem: ""
+        schedItem: "new item at 12"
     },
     {
         hourBlock: 1,
-        schedItem: ""
+        schedItem: "new item at 1"
     },
     {
         hourBlock: 2,
-        schedItem: ""
+        schedItem: "new item at 2"
     },
     {
         hourBlock: 3,
-        schedItem: ""
+        schedItem: "new item at 3"
     },
     {
         hourBlock: 4,
-        schedItem: ""
+        schedItem: "new item at 4"
     },
     {
         hourBlock: 5,
-        schedItem: ""
+        schedItem: "new item at 5"
     },
 ]
+console.log(Object.values(timeChunks));
+
+
+
+
+
 function createRow(){
 var timeBLockEl = $("<div>", {"class": "time-block"});
 var rowEl = $("<div>", {"class": "row"});
-var hourEl = $("<div>", {"class": "hour"});
-var descriptionEl = $("<div>", {"class": "description"});
+hourEl = $("<div>", {"class": "hour"});
+descriptionEl = $("<form>", {"class": "description"});
 var saveBtnEl = $("<div>", {"class": "saveBtn"});
 var saveButton = $("<i>", {"class": "far fa-save"});
 
@@ -67,15 +73,15 @@ timeBLockEl.append(rowEl);
 $(".container").append(timeBLockEl);
 
 }
+// createRow(); 
 
-createRow();
-// function populateCal() {
-//     timeChunk.hourBlock.each(function (index){
-//         $(".hour").text = hourBlock;
-//     })
-// }
-    
-// populateCal;   
+timeChunks.forEach((timeChunk) =>
+    createRow(hourEl.text(`${timeChunk.hourBlock} AM`), descriptionEl.text(`${timeChunk.schedItem}`))
+);
+// timeChunks.forEach((timeChunk) =>
+// hourEl.text(`${timeChunk.hourBlock} AM`)
+// );
+
 
 //Need to populate cells with hour and event details (from timeChunks array? and localStorage)
 //need to show if event block is past, present, or future (hourBlock < currentHour, >, or else?)
@@ -83,4 +89,13 @@ createRow();
 //save text from input form when cal icon clicked--> send to localStorage
 
 
+//example from https://www.reddit.com/r/javascript/comments/8hl06s/how_to_properly_access_objects_in_array/
+// var donuts = [
+//     { type: "Jelly", cost: 1.22 },
+//     { type: "Chocolate", cost: 2.45 },
+//     { type: "Cider", cost: 1.59 },
+//     { type: "Boston Cream", cost: 5.99 }
+//     ];
+
+// donuts.forEach((donut) => console.log(`${donut.type} donuts cost $${donut.cost} each`));
 
